919c380db17c7144559a1a1295a2c387ae2e83b4
sugar <amirmohammad.rzn121@gmail.com>
2026-01-05 00:12:29 +0330

    adding application context

---
+++ src/main/java/ir/Hw13/util/ApplicationContext.java
@@ -0,0 +1,37 @@
+package ir.Hw13.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Objects;
+
+public class ApplicationContext {
+    private static ApplicationContext context;
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    private ApplicationContext(){}
+
+    public static ApplicationContext getInstance(){
+        if (Objects.isNull(context)){
+            context = new ApplicationContext();
+        }
+        return context;
+    }
+
+    private EntityManagerFactory getEntityManagerFactory(){
+        if (Objects.isNull(entityManagerFactory)){
+            entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        }
+        return entityManagerFactory;
+    }
+
+    private EntityManager getEntityManager(){
+        if (Objects.isNull(entityManager)){
+            entityManager = getEntityManagerFactory().createEntityManager();
+        }
+        return entityManager;
+    }
+
+}
